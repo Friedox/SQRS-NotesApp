f685c770d14b6c5f0b1d1789c8902d3983675aac
EzzySoft <daniyatretyakov@gmail.com>
2025-05-05 09:39:23 +0300

    [SQR] feat: setup alembic migrations

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,23 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 load_dotenv()
 
 
+class DatabaseConfig(BaseModel):
+    db_url: str
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 10
+    pool_size: int = 50
+
+    naming_convention: dict[str, str] = {
+        "ix": "ix_%(column_0_label)s",
+        "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+        "ck": "ck_%(table_name)s_%(constraint_name)s",
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+        "pk": "pk_%(table_name)s",
+    }
+    model_config = ConfigDict(extra="ignore")
+
+
 class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
@@ -15,6 +32,7 @@ class RunConfig(BaseModel):
 class ApiV1Prefix(BaseModel):
     prefix: str = "/v1"
     status_prefix: str = "/status"
+    auth_prefix: str = "/auth"
 
 
 class ApiPrefixConfig(BaseModel):
@@ -32,6 +50,8 @@ class Settings(BaseSettings):
     run: RunConfig
 
     api: ApiPrefixConfig = ApiPrefixConfig()
+
+    database: DatabaseConfig
 
     BaseSettings.model_config = ConfigDict(extra="ignore")
 
