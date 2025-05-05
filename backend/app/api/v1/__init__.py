4f0eb277a96853b35e4c35c18c18ac27cb2c0145
Mikhail K <mikhail_kalinin_a@mail.ru>
2025-05-05 18:20:03 +0300

    Previous functionality tweaks

--- backend/app/api/v1/__init__.py
+++ backend/app/api/v1/__init__.py
@@ -5,9 +5,7 @@ from config import settings
 from .auth import router as auth_router
 from .status import router as status_router
 
-
 router = APIRouter(prefix=settings.api.v1.prefix)
 
-
 router.include_router(status_router, prefix=settings.api.v1.status_prefix)
-router.include_router(auth_router, prefix=settings.api.v1.status_prefix)
+router.include_router(auth_router, prefix=settings.api.v1.auth_prefix)
