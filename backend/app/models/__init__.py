91ff0dd233809bd49ad2824ecf601f481433b729
EzzySoft <daniyatretyakov@gmail.com>
2025-05-05 21:25:51 +0300

    [SQR] feat: setup `jwt_token` table to manage token revocation

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["Base", "User", "database_helper"]
+__all__ = ["Base", "User", "database_helper", "Token"]
 
 from .base import Base
 from .helper import database_helper
+from .token import Token
 from .user import User
